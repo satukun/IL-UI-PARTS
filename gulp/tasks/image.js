6591c56fe1ea942b49058a3ee32d1a6ed8b02ef9
佐藤 陽介 <ysato@C0RNM011.local>
2017-02-10 16:01:27 +0900

    変更

--- gulp/tasks/image.js
+++ gulp/tasks/image.js
@@ -7,56 +7,56 @@ var merge = require('merge-stream');
 var pngquant = require('imagemin-pngquant');
 var version = require('../config').version;
 
-gulp.task('sprite:pc', function () {
-  return sprite('pc')
+gulp.task('sprite:pc', function() {
+    return sprite('pc')
 });
 
-gulp.task('sprite:sp', function () {
-  return sprite('sp')
+gulp.task('sprite:sp', function() {
+    return sprite('sp')
 });
 
-gulp.task('img:pc', function () {
-  return image('pc');
+gulp.task('img:pc', function() {
+    return image('pc');
 });
 
-gulp.task('img:sp', function () {
-  return image('sp');
+gulp.task('img:sp', function() {
+    return image('sp');
 });
 
 function image(device) {
-  return gulp.src('./src/img/' + device + '/*.png')
-  .pipe(imagemin({
-    progressive: true,
-    svgoPlugins: [{removeViewBox: false}],
-    use: [pngquant()]
-  }))
-  .pipe(gulp.dest('./server/public/img/' + device));
+    return gulp.src('./src/image/' + device + '/*.png')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false }],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('./server/public/image/' + device));
 }
 
 
 function sprite(device) {
-  var spriteData = gulp.src('./src/img/' + device + '/sprites/*.png')
-  .pipe(spritesmith({
-    imgName: 'sprite_' + version + '.png',
-    cssName: '_sprite.scss',
-    imgPath: '/img/' + device + '/sprite_' + version + '.png',
-    cssFormat: 'scss',
-    cssVarMap: function (sprite) {
-      sprite.name = 'sprite-' + sprite.name;
-    }
-  }));
+    var spriteData = gulp.src('./src/image/' + device + '/sprites/*.png')
+        .pipe(spritesmith({
+            imgName: 'sprite_' + version + '.png',
+            cssName: '_sprite.scss',
+            imgPath: '/image/' + device + '/sprite_' + version + '.png',
+            cssFormat: 'scss',
+            cssVarMap: function(sprite) {
+                sprite.name = 'sprite-' + sprite.name;
+            }
+        }));
 
-  var imgStream = spriteData.img
-    .pipe(buffer())
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{removeViewBox: false}],
-      use: [pngquant()]
-    }))
-    .pipe(gulp.dest('./server/public/img/' + device));
+    var imgStream = spriteData.img
+        .pipe(buffer())
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false }],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('./server/public/image/' + device));
 
-  var cssStream = spriteData.css
-    .pipe(gulp.dest('./src/css/' + device + '/foundation'));
+    var cssStream = spriteData.css
+        .pipe(gulp.dest('./src/css/' + device + '/foundation'));
 
-  return merge(imgStream, cssStream);
+    return merge(imgStream, cssStream);
 }
